1c405eff0c68aa3f78b1e5103228e73ce343ab58
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-02-29 16:51:30 +0800

    fix settings boss branch

--- resources/views/settings/password.blade.php
+++ resources/views/settings/password.blade.php
@@ -2,6 +2,20 @@
 
 @section('title', ' - Password')
 
+@section('navbar-2')
+<ul class="nav navbar-nav navbar-right"> 
+  <li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+      <span class="glyphicon glyphicon-menu-hamburger"></span>
+    </a>
+    <ul class="dropdown-menu">
+      <li><a href="/settings"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
+      <li><a href="/logout"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>     
+    </ul>
+  </li>
+</ul>
+<p class="navbar-text navbar-right">{{ $name }}</p>
+@endsection
 
 @section('container-body')
 <div class="container-fluid">

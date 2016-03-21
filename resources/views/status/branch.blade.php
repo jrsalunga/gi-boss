d3adafc1101eb41004e0832fcd37cb900ddc487c
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-03-21 18:14:14 +0800

    fix branch

--- resources/views/status/branch.blade.php
+++ resources/views/status/branch.blade.php
@@ -415,7 +415,7 @@
         type: 'line',
         spacingRight: 0,
         marginTop: 40,
-        //marginRight: 30,
+        marginRight: 10,
         zoomType: 'x',
         panning: true,
         panKey: 'shift'

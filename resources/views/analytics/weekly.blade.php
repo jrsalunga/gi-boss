2c32fb49bed0ca154494569a6f04188f62e15c1a
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-05-11 16:51:59 +0800

    fix all branch dp

--- resources/views/analytics/weekly.blade.php
+++ resources/views/analytics/weekly.blade.php
@@ -282,12 +282,12 @@
               ?>
               <td class="text-right" data-sort="{{ number_format($mancost,2,'.','') }}">{{ number_format($mancost,2) }}</td>
               <td class="text-right" data-sort="{{ $d->dailysale['mancostpct'] }}"
-                @if(!empty($d->dailysale['sales']) && $d->dailysale['sales']!='0.00' && $d->dailysale['sales']!='0')   
+                @if(!empty($d->dailysale['sales']) && $d->dailysale['sales']!='0.00' && $d->dailysale['sales']!=0)   
                 title="({{$d->dailysale['empcount']}}*{{$branch->mancost}})/{{$d->dailysale['sales']}} 
-                ={{(($d->dailysale['empcount']*$branch->mancost)/1)*100}} "
+                ={{(($d->dailysale['empcount']*$branch->mancost)/$d->dailysale['sales'])*100}} "
                 @endif
                 >
-                {{ number_format((($d->dailysale['empcount']*$branch->mancost)/1)*100, 2)}}
+                {{ number_format((($d->dailysale['empcount']*$branch->mancost)/$d->dailysale['sales'])*100, 2)}}
               </td>
               <td class="text-right" data-sort="{{ number_format($d->dailysale['tips'],2,'.','') }}">{{ number_format($d->dailysale['tips'],2) }}</td>
               <?php

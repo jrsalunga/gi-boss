71b0d6ee48e00c2cb6c1477d40326e1eb81f6f03
Jefferson Salunga <jefferson.salunga@gmail.com>
2017-06-13 15:48:26 +0800

    fix

--- resources/views/product/sales/comparative.blade.php
+++ resources/views/product/sales/comparative.blade.php
@@ -146,7 +146,7 @@
             <td class="text-right">{{ number_format($data->grsamt, 2) }}</td>  
             <td class="text-right">
               @if($data->neg_qty<0)
-              <span class="help" data-toggle="tooltip" title="{{ $data->trans_cnt }}{{ $data->neg_qty+0 }}">
+              <span class="help" data-toggle="tooltip" title="{{ $data->trans_cnt }}{{ ($data->neg_qty*2)+0 }}">
                 {{ number_format($data->trans_actual, 0) }}
               </span>
               @else

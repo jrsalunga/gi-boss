bff1a56962889032aeb41e9d4d2582a0df34613d
Jefferson Salunga <jefferson.salunga@gmail.com>
2017-07-14 18:14:00 +0800

    fix

--- resources/views/dashboard/watchlist.blade.php
+++ resources/views/dashboard/watchlist.blade.php
@@ -100,16 +100,12 @@
                     @if(count($d['timelogs'])>0)
                       @foreach($d['timelogs'] as $timelog)
                       <span>
-                        <span class="help label label-{{ $timelog->txnClass() }}" data-toggle="tooltip" title="{{ $timelog->getTxnCode() }} {{ $timelog->datetime->format('g:i:s A') }}">
-                          {{ $timelog->datetime->format('g:i A') }}
-                          @
-                          {{ $timelog->branch->code or '' }}
-
-                          <span class="help label label-{{ $timelog->entryClass() }}" data-toggle="tooltip" title="{{ $timelog->getEntry() }}">
+                        <span class="help label label-{{ $timelog->txnClass() }}" data-toggle="tooltip" title="{{ $timelog->getEntry() }} {{ $timelog->getTxnCode() }} {{ $timelog->datetime->format('g:i:s A') }}">
+                          {{ $timelog->datetime->format('g:i A') }} @ {{ $timelog->branch->code or '' }}
+                          <span class="help label label-{{ $timelog->entryClass() }}" >
                             {{ $timelog->getEntry() }}
                           </span>
                         </span>
-                        
                       </span>
                       &nbsp;
                       @endforeach

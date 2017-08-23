46f2f622f1b1c2b21c99f19f8e75f638910a2867
Jefferson Salunga <jefferson.salunga@gmail.com>
2017-08-23 18:54:08 +0800

    fix

--- resources/views/docu/depslp/checklist2.blade.php
+++ resources/views/docu/depslp/checklist2.blade.php
@@ -183,8 +183,9 @@
           </td>
           <td class="{{ $bg }} text-center">
             @if($b['depo_totamt']>0 || $b['pos_totamt']>0)
-              @if(floatval($b['depo_totamt'])==floatval($b['pos_totamt']))
+              @if($b['depo_totamt']==$b['pos_totamt'])
                 <span class="glyphicon glyphicon-ok text-success"></span>
+                {{ $b['depo_totamt'] }} = {{ $b['pos_totamt'] }}
               @else
                 <span class="glyphicon glyphicon-remove text-danger"></span>
               @endif

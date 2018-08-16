d036cc049a352e17d900f2e1a7569a5cf2098191
Jefferson Salunga <jefferson.salunga@gmail.com>
2018-08-16 15:25:01 +0800

    fix

--- resources/views/masterfiles/branch/table.blade.php
+++ resources/views/masterfiles/branch/table.blade.php
@@ -1,6 +1,16 @@
 <tr>
 	<td>
-		<a href="/masterfiles/branch/{{ strtolower($branch->code) }}">{{ $branch->code }}</a> - 
-		<a href="/masterfiles/branch/{{ $branch->lid() }}">{{ $branch->descriptor }}</a>
+		<a class="{{ $branch->status=='3'?'text-muted':(in_array($branch->type, ['3', '4'])?'text-primary':'text-sucess') }}" href="/masterfiles/branch/{{ strtolower($branch->code) }}">{{ $branch->code }}</a> - 
+		<a class="{{ $branch->status=='3'?'text-muted':(in_array($branch->type, ['3', '4'])?'text-primary':'text-sucess') }}" href="/masterfiles/branch/{{ $branch->lid() }}">{{ $branch->descriptor }}</a>
+	</td>
+	<td>
+		@if(isset($branch->company))
+			<a href="/masterfiles/company/{{ $branch->company->lid() }}">{{ $branch->company->code }}</a>
+		@endif
+	</td>
+	<td>
+		@if(isset($branch->lessor))
+			<a href="/masterfiles/lessor/{{ $branch->lessor->lid() }}">{{ $branch->lessor->code }}</a>
+		@endif
 	</td>
 </tr>

2d9ac05485440b62a643053e8626e762ccde980f
Jefferson Salunga <jefferson.salunga@gmail.com>
2018-08-16 12:58:34 +0800

    fix

--- app/Http/Controllers/Hr/EmployeeController.php
+++ app/Http/Controllers/Hr/EmployeeController.php
@@ -783,7 +783,7 @@ class EmployeeController extends Controller
 						->with('employee', $employee)
 						->with('departments', $this->department->all())
 						->with('companies', $this->company->all())
-						->with('branches', $this->branch->orderBy('code')->all())
+						->with('branches', $this->branch->skipCache()->orderBy('code')->all())
 						->with('positions', $this->position->all());
 	}
 

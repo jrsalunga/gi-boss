b527358753a5007855a8da4b1a53d44696256ca9
Jefferson Salunga <jefferson.salunga@gmail.com>
2018-09-11 13:29:06 +0800

    fix

--- resources/views/docu/setslp/view.blade.php
+++ resources/views/docu/setslp/view.blade.php
@@ -83,7 +83,7 @@
             <h5><span class="gly gly-user"></span> {{ $setslp->cashier }}</h5>
             <h6><span class="gly gly-pencil"></span> {{ $setslp->remarks }}</h6>
             
-            <h6><span class="glyphicon glyphicon-cloud-upload"></span> {{ $setslp->created_at->format('D M j h:i:s A') }} <small>{{ diffForHumans($setslp->created_at) }}</small></h6>
+            <h6><span class="glyphicon glyphicon-cloud-upload"></span> {{ $setslp->created_at->format('D M j H:i:s') }} <small>{{ diffForHumans($setslp->created_at) }}</small></h6>
           </div>
           <div class="panel-footer">
             @if($setslp->verified || $setslp->matched)

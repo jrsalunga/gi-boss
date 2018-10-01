0aba6a94cb231bfc529c681274a4d3ef1cb86c14
Jefferson Salunga <jefferson.salunga@gmail.com>
2018-10-01 14:31:33 +0800

    fix

--- resources/views/component/transfer/daily-summary.blade.php
+++ resources/views/component/transfer/daily-summary.blade.php
@@ -44,15 +44,6 @@
                 <span>{{ $branch->code }}</span>
               @endif
 
-              @if(empty(($filter->item)) || is_null(($branch)))
-                
-              @else
-              <span class="label label-info" style="margin-left: 5px;" data-toggle="loader">
-                {{ $filter->item }} 
-                <a href="/component/transfer?branchid={{strtolower($branch->id)}}&amp;to={{$dr->to->format('Y-m-d')}}&amp;fr={{$dr->fr->format('Y-m-d')}}" title="Remove filter">
-                <span style="color:#ccc; margin-right: 5px; border-radius: .25em;">x</span>
-                </a>
-              </span>
               @endif
 
               

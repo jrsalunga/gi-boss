74d1b8e2ef3529431b66e160df9464ebe66994c1
Jefferson Salunga <jefferson.salunga@gmail.com>
2018-12-17 17:22:43 +0800

    fix

--- resources/views/hr/masterfiles/employee/edit-employment.blade.php
+++ resources/views/hr/masterfiles/employee/edit-employment.blade.php
@@ -24,10 +24,7 @@
 	</div>
 </div>
 <h3 class="text-success" style="margin: 15px 0;"> 
-	<a href="/hr/masterfiles/employee/{{ $employee->lid() }}">
-		{{ $employee->lastname }}, {{ $employee->firstname }} {{ $employee->middlename }} 
-	</a>
-	<a href="/hr/masterfiles/employee/{{ $employee->lid() }}">
+	<a href="/hr/masterfiles/employee/{{ $employee->lid() }}">{{ $employee->lastname }}, {{ $employee->firstname }} {{ $employee->middlename }}</a>
 	<small data-id="{{ $employee->id }}">{{ $employee->code }}</small>
 </h3>
 <form action="/hr/masterfiles/employee" method="POST">

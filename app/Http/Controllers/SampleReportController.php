b0845301c14ca79cc010f0c8c7e2ebd0b9b92430
Jefferson Salunga <jefferson.salunga@yahoo.com>
2020-01-15 19:44:03 +0800

    fix

--- app/Http/Controllers/SampleReportController.php
+++ app/Http/Controllers/SampleReportController.php
@@ -44,7 +44,7 @@ class SampleReportController extends Controller
   	$datas = [];
 
   	$branches = \App\Models\Boss\Branch::whereStatus('2')->whereIn('type', ['0', '1', '2', '3'])->orderBy('code')->get();
-  	$components = \App\Models\Component::whereIn('compcatid', ['3DF8FB71636311E5B83800FF59FBB323'])
+  	$components = \App\Models\Component::whereIn('compcatid', ['3DE82C56636311E5B83800FF59FBB323'])
   																		->orderBy('descriptor')->get();
 
 
@@ -62,7 +62,7 @@ class SampleReportController extends Controller
 
   		$mcs = $this->mComponent->skipCache()->scopeQuery(function($query) use ($branch){ 
   												return $query->leftJoin('component', 'component.id', '=', 'month_component.component_id')
-  																		->whereIn('component.compcatid', ['3DF8FB71636311E5B83800FF59FBB323'])
+  																		->whereIn('component.compcatid', ['3DE82C56636311E5B83800FF59FBB323'])
   																		->where('month_component.branch_id', $branch->id)
   																		->where('month_component.date', '2018-10-31');
   												})->all();
@@ -83,6 +83,70 @@ class SampleReportController extends Controller
   	return view('sample-report.meat')
   							->with('datas', $datas)
   							->with('components', $components);
+  }
+
+
+  public function getProdcat(Request $request) {
+    $branch = new \App\Models\Boss\Branch;
+    $prodcat = new \App\Models\Prodcat;
+    $mprodcat = new \App\Models\MonthProdcat;
+    $ds = new \App\Models\MonthlySales;
+
+
+
+    $branches = $branch
+        ->select('code', 'descriptor', 'id')
+        ->where('status', 2)
+        ->where('type', '<', 4)
+        ->orderBy('code')
+        ->get();
+
+    $prodcats = $prodcat
+        ->where('ordinal', '>', 0)
+        ->orderBy('ordinal')
+        ->get();
+
+
+    $datas = [];
+
+    foreach ($branches as $k => $b) {
+      foreach (['2019-06-30', '2019-07-31'] as $l => $d) {
+        $dss = $ds->where('branch_id', $b->id)->where('date', $d)->first();
+        $date = Carbon::parse($d);
+        
+        $datas[$b->code][$l]['date'] = $date;
+
+        
+        if (is_null($dss)) {
+          $datas[$b->code][$l]['sales'] = 0;
+          $datas[$b->code][$l]['gross'] = 0;
+          $datas[$b->code][$l]['food_sales'] = 0;
+          $datas[$b->code][$l]['cos'] = 0;
+          $datas[$b->code][$l]['cospct'] = 0;
+          $datas[$b->code][$l]['purchcost'] = 0;
+          $datas[$b->code][$l]['transcost'] = 0;
+          $datas[$b->code][$l]['transcos'] = 0;
+          $datas[$b->code][$l]['transncos'] = 0;
+          $datas[$b->code][$l]['emp_meal'] = 0;
+          $datas[$b->code][$l]['opex'] = 0;
+        } else {
+          $datas[$b->code][$l]['sales'] = $dss->sales;
+          $datas[$b->code][$l]['gross'] = $dss->slsmtd_totgrs;
+          $datas[$b->code][$l]['food_sales'] = $dss->food_sales;
+          $datas[$b->code][$l]['cos'] = $dss->cos;
+          $datas[$b->code][$l]['cospct'] = $dss->fc;
+          $datas[$b->code][$l]['purchcost'] = $dss->purchcost;
+          $datas[$b->code][$l]['transcost'] = $dss->transcost;
+          $datas[$b->code][$l]['transcos'] = $dss->transcos;
+          $datas[$b->code][$l]['transncos'] = $dss->transncos;
+          $datas[$b->code][$l]['emp_meal'] = $dss->emp_meal;
+          $datas[$b->code][$l]['opex'] = $dss->opex;
+        }
+      }
+    }
+    
+   return view('sample-report.prodcat')
+                ->with('datas', $datas);
   }
 
   

94aecacc09a83c9f0fa0bd27461a2e61e46d284b
Jefferson Salunga <jefferson.salunga@yahoo.com>
2020-06-25 14:50:10 +0800

    fix

--- resources/views/report/cash-audit.blade.php
+++ resources/views/report/cash-audit.blade.php
@@ -116,7 +116,9 @@
     <div class="row">
       <div class="col-md-12" style="margin-top: 20px;">
       @if(is_null($cash_audit))
+        @if(is_null($branch))
         No Data
+        @endif
       @else
         <div class="panel panel-default">
           <div class="panel-heading">Cash Flow</div>

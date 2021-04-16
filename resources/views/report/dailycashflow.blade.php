8aa6fd42cc066efca31f4b6eb1a185d9e35b3c81
Jefferson Salunga <jefferson.salunga@yahoo.com>
2021-04-16 17:10:08 +0800

    fix

--- resources/views/report/dailycashflow.blade.php
+++ resources/views/report/dailycashflow.blade.php
@@ -95,6 +95,7 @@
           <th class="text-right">Cash Sale</th>
           <th class="text-right"><span data-toggle="tooltip" title="Change Fund + Cash Sale">Cash Total</span></th>
           <th class="text-right">Cash Disbursement</th>
+          <th class="text-right">Cash Out/Refund</th>
           <th class="text-right">Ending Balance</th>
           <th class="text-right">Actual Cash</th>
           <th class="text-right">(Short)/Over</th>
@@ -117,6 +118,7 @@
           $tot_shrt_cumm = 0;
           $tot_chg_sale = 0;
           $tot_pos_sales = 0;
+          $tot_tot_out = 0;
           $ctr = 0;
 
           // ['csh_fwdd', 'deposit', 'csh_sale', 'chg_sale', 'csh_disb', 'csh_bal', 'csh_cnt', 'shrt_ovr', 'shrt_cumm']
@@ -132,6 +134,7 @@
             </a>
           </td>
           @if(is_null($data['cash_audit']))
+            <td class="text-right">-</td>
             <td class="text-right">-</td>
             <td class="text-right">-</td>
             <td class="text-right">-</td>
@@ -177,6 +180,7 @@
               $tot_shrt_cumm    += $data['cash_audit']['shrt_cumm'];
               $tot_chg_sale     += $data['cash_audit']['chg_sale'];
               $tot_pos_sales    += $data['cash_audit']['pos_sales'];
+              $tot_tot_out      += $data['cash_audit']['tot_out'];
 
               $ctr++;
 
@@ -218,6 +222,9 @@
               <a href="/component/purchases?table=payment&item=Cash&itemid=c&branchid={{ stl($data['branch_id']) }}&fr={{ $dr->date->format('Y-m-d') }}&to={{ $dr->date->format('Y-m-d') }}" target="_blank"  data-toggle="tooltip" title="view purchase log page">
                 {{ nf($data['cash_audit']['csh_disb']) }}
               </a>
+            </td>
+            <td class="text-right" data-sort="{{ $data['cash_audit']['tot_out']>0?nf($data['cash_audit']['tot_out'],0):'' }}">
+              {{ nf($data['cash_audit']['tot_out']) }}
             </td>
             <td class="text-right" data-sort="{{ $data['cash_audit']['csh_bal']>0?nf($data['cash_audit']['csh_bal'],0):'' }}">
               {{ nf($data['cash_audit']['csh_bal']) }}
@@ -299,6 +306,11 @@
           <td class="text-right">
             <strong>
               {{ number_format($tot_csh_disb,2) }}
+            </strong>
+          </td>
+          <td class="text-right">
+            <strong>
+              {{ number_format($tot_tot_out,2) }}
             </strong>
           </td>
           <td class="text-right">

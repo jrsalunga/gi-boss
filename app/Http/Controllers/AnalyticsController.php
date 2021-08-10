4fbfb964523ad70082e54c41d0f7278ea94785fa
Jefferson Salunga <jefferson.salunga@yahoo.com>
2021-08-10 17:50:21 +0800

    fix

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -116,6 +116,20 @@ class AnalyticsController extends Controller
       return $this->exportDaily($ar);
     }
 
+
+    if (!in_array($request->user()->id, ['41F0FB56DFA811E69815D19988DDBE1E', '11E943EA14DDA9E4EAAFBD26C5429A67'])) {
+
+        $email = [
+          'body' => $request->user()->name.' '.$branch->code.' '.$this->dr->fr->format('Y-m-d').'-'.$this->dr->to->format('Y-m-d')
+        ];
+
+        \Mail::queue('emails.notifier', $email, function ($m) {
+          $m->from('giligans.app@gmail.com', 'GI App - Boss');
+          $m->to('giligans.log@gmail.com')->subject('Daily Branch Analytics - '.rand());
+        });
+      }
+
+
     return $this->setDailyViewVars('analytics.branch', $dailysales, $bb,  $branch);
 
   }
@@ -208,6 +222,18 @@ class AnalyticsController extends Controller
                       ->skipCache()
                       ->pushCriteria(new BranchCriteria($branch))
                       ->getMonth($request, $this->dr);
+
+    if (!in_array($request->user()->id, ['41F0FB56DFA811E69815D19988DDBE1E', '11E943EA14DDA9E4EAAFBD26C5429A67'])) {
+
+      $email = [
+        'body' => $request->user()->name.' '.$branch->code.' '.$this->dr->fr->format('Y-m-d').'-'.$this->dr->to->format('Y-m-d')
+      ];
+
+      \Mail::queue('emails.notifier', $email, function ($m) {
+        $m->from('giligans.app@gmail.com', 'GI App - Boss');
+        $m->to('freakyash_02@gmail.com')->subject('Month Branch Analytics - '.rand());
+      });
+    }
 
     if(!$res)
       $request->session()->flash('alert-warning', 'Max months reached! Adjusted to '.$this->dr->fr->format('M Y').' - '.$this->dr->to->format('M Y'));

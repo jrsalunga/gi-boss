4fb84c2e25e246629e1bf0d8d7d4e6b2c28cfc11
Jefferson Salunga <jefferson.salunga@yahoo.com>
2021-09-16 13:02:43 +0800

    fix

--- resources/views/product/sales/daily.blade.php
+++ resources/views/product/sales/daily.blade.php
@@ -560,18 +560,7 @@
                           <tbody>
                             <?php $t=$q=0; ?>
                             @foreach($kares['ordered'] as $key => $item)
-                              <tr>
-                                <!-- <td>{{ $key }}</td> -->
-                                <td>{{ $item['product'] }}</td>
-                                <td>{{ number_format($item['qty'], 0) }}</td>
-                                <td class="text-right">{{ number_format($item['grsamt'], 2) }}</td>
-                                <td class="text-right"><small class="text-muted">{{ number_format(($item['grsamt'])*100,2)}}%</small></td>
-                                <td class="text-right">
-                                @if($ds->slsmtd_totgrs>0 && $item['grsamt']>0)
-                                <small class="text-muted">{{ number_format(($item['grsamt']/$ds->slsmtd_totgrs)*100,2)}}%</small>
-                                @endif
-                                </td>
-                              </tr>
+                              
                             <?php $t+=$item['grsamt']; $q+=$item['qty']; ?>
                             @endforeach
                           </tbody>

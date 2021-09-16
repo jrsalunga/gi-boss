964c2d5903cd3577a51c60c015b0261d51b26a63
Jefferson Salunga <jefferson.salunga@yahoo.com>
2021-09-16 12:57:30 +0800

    fix

--- resources/views/product/sales/daily.blade.php
+++ resources/views/product/sales/daily.blade.php
@@ -565,10 +565,10 @@
                                 <td>{{ $item['product'] }}</td>
                                 <td>{{ number_format($item['qty'], 0) }}</td>
                                 <td class="text-right">{{ number_format($item['grsamt'], 2) }}</td>
-                                <td class="text-right"><small class="text-muted">{{ number_format(($item['grsamt'])*100,2)}}%</small></td>
+                                <td class="text-right"><small class="text-muted">{{ number_format(($item['grsamt']/$tm)*100,2)}}%</small></td>
                                 <td class="text-right">
                                 @if($ds->slsmtd_totgrs>0)
-                                <small class="text-muted">{{ number_format(($item['grsamt'])*100,2)}}% SSS {{ $ds->slsmtd_totgrs }}</small>
+                                <small class="text-muted">{{ number_format(($item['grsamt']/$ds->slsmtd_totgrs)*100,2)}}%</small>
                                 @endif
                                 </td>
                               </tr>

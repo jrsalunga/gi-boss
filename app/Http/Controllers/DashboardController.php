3669086eb259dde2e1704749573daf655fff6249
Jefferson Salunga <jefferson.salunga@yahoo.com>
2024-02-16 15:25:41 +0800

    fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -50,8 +50,8 @@ class DashboardController extends Controller
 
 	private function delinquent(Request $request){
 		// $branchs = Branch::orderBy('code')->get(['code', 'descriptor', 'id']);
-		// $branchs = $this->branch->all(['code', 'descriptor', 'id']);
-    $branchs = $this->bbranch->where('status', 1)->get(['code', 'descriptor', 'id']);
+		$branchs = $this->branch->all(['code', 'descriptor', 'id']);
+    // $branchs = $this->bbranch->where('status', 1)->get(['code', 'descriptor', 'id']);
 	
 		$arr = [];
 		$arr_wl = [];
